8b5ac40c34a6b19751db93de13b8a8f61af0a31a
Matt Carver <mattcarver.wd@gmail.com>
2021-12-16 20:16:41 -0800

    entering js

---
+++ script.js
@@ -0,0 +1,12 @@
+const musicContainer = document.getElementByIdI('music_container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+const songs = ['hey', 'summer'];
