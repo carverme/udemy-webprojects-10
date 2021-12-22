f352e1e1878dafb5e2a12a3a93dbe1d25f4f9118
Matt Carver <mattcarver.wd@gmail.com>
2021-12-22 13:38:17 -0800

    updating js

--- script.js
+++ script.js
@@ -9,4 +9,50 @@ const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 
-const songs = ['hey', 'summer'];
+// Song titles
+const songs = ['hey', 'summer', 'ukelele'];
+
+//Keep track of song
+let songIndex = 2;
+
+//Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+}
+
+//Play Song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+
+//Pause Song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains(
+    'play'
+    );
+
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
